02909622f980f8194d1140ad16530f210300e1bc
Shaik Rehman <shaikrehman78609@gmail.com>
2025-06-20 13:48:43 +0530

    Update cart.js

--- frontend/src/services/cart.js
+++ frontend/src/services/cart.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:5000/api/cart";
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api/cart`;
+
 
 const authHeader = (token) => ({
   "Content-Type": "application/json",
